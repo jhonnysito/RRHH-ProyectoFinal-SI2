e33827afb8a8f837ec6d7f87001c69693570e967
raul <raulalbertoaguilar01@gmail.com>
2025-11-21 03:12:18 -0400

    correcion en controller el rol

--- app/Http/Controllers/PermisoEmpleadoController.php
+++ app/Http/Controllers/PermisoEmpleadoController.php
@@ -108,7 +108,7 @@ class PermisoEmpleadoController extends Controller
             ]);
 
             // NOTIFICAR ADMINISTRADORES
-            $administradores = User::role('Administrador')->get();
+            $administradores = User::role('Admin')->get();
             foreach ($administradores as $admin) {
                 $admin->notify(new PermisosNotification($permiso, 'solicitado'));
             }
